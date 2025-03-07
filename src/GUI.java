ef7c1bb706918debda5553088046214842ed0bd5
Mpoked <pokorny_marek@oauh.cz>
2025-03-07 13:15:39 +0100

    už to funguje

--- src/GUI.java
+++ src/GUI.java
@@ -11,7 +11,38 @@ public class GUI extends JFrame {
         setSize(400, 300);
 
 
+        JMenuBar menuBar = new JMenuBar();
+
+        // Vytvoření menu
         JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu helpMenu = new JMenu("Help");
+
+        // Vytvoření položek menu
         JMenuItem newItem = new JMenuItem("New");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        // Přidání položek do menu
+        fileMenu.add(newItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitItem);
+
+        // Přidání menu do menu baru
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+
+        // Nastavení menu baru do hlavního okna
+        setJMenuBar(menuBar);
+
+        newItem.addActionListener(e -> zapis());
+    }
+
+    public void zapis(){
+        txArea.append("Ahoj\n");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-public class Main{
-    public static void main(String[] args){
-        GUI gui =  new GUI();
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
         gui.setVisible(true);
     }
 }
